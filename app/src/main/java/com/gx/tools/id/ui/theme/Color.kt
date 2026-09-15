7d21b106545d557ee31bb026eef130701e6cbd96
george555rr-byte <george555rr@gmail.com>
2026-09-15 23:00:32 +0300

    Add Color.kt with theme color definitions

---
+++ app/src/main/java/com/gx/tools/id/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.gx.tools.id.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BgDark       = Color(0xFF0A0A0F)
+val SurfaceDark  = Color(0xFF121218)
+val BorderPurple = Color(0xFF2A1F4E)
+val NeonPurple   = Color(0xFFB44BFF)
+val NeonPurple2  = Color(0xFF8A2BE2)
+val TextWhite    = Color(0xFFF0F0F0)
+val TextGray     = Color(0xFF9A9AA8)
+val AccentGreen  = Color(0xFF00E676)
+val AccentCyan   = Color(0xFF00E5FF)
+val AccentOrange = Color(0xFFFF9800)
+val AccentAmber  = Color(0xFFFFC107)
